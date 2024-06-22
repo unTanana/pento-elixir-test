ef81ea15bf957643bd34d44c0489c923311cdb08
Ciprian Tanana <ciprian.tanana@gmail.com>
2024-06-22 14:49:45 +0300

    fix tests

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,13 @@ defmodule Pento.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
+  def valid_username, do: "LeUsername"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password()
+      password: valid_user_password(),
+      username: valid_username()
     })
   end
 
